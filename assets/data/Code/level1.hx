025b7cceea1e5363e6f099cd0f05bf10d5960f79
NPALLRussell <russellm@npallaudio.com>
2024-10-15 16:13:46 -0400

    Init Upload

---
+++ assets/data/Code/level1.hx
@@ -0,0 +1,124 @@
+//EASY
+
+//Round 1
+_Enemies =
+[ 
+["units" => "1", "name" => "Skeleton", "hpscale" => "1", "delay" => "20"],
+["units" => "2", "name" => "Skeleton", "hpscale" => "1", "delay" => "24"],
+["units" => "1", "name" => "Skeleton", "hpscale" => "1", "delay" => "5"],
+["units" => "1", "name" => "Skeleton", "hpscale" => "1", "delay" => "0"],
+];
+
+//Round 2
+_Enemies =
+[ 
+["units" => "1", "name" => "Armored Skeleton", "hpscale" => "1",  "delay" => "4"],
+["units" => "1", "name" => "Armored Skeleton", "hpscale" => "1",  "delay" => "24"],
+["units" => "1", "name" => "Armored Skeleton", "hpscale" => "1",  "delay" => "2"],
+["units" => "1", "name" => "Armored Skeleton", "hpscale" => "1",  "delay" => "5"],
+["units" => "1", "name" => "Armored Skeleton", "hpscale" => "1",  "delay" => "0"],
+];
+
+//Round 3
+_Enemies =
+[ 
+["units" => "1", "name" => "Skeleton", "hpscale" => "1", "delay" => "4"],
+["units" => "1", "name" => "Armored Skeleton", "hpscale" => "1", "delay" => "24"],
+["units" => "2", "name" => "Skeleton", "hpscale" => "1", "delay" => "15"],
+["units" => "1", "name" => "Armored Skeleton", "hpscale" => "1", "delay" => "8"],
+["units" => "1", "name" => "Skeleton", "hpscale" => "2", "delay" => "0"],
+];
+
+//Round 4
+_Enemies =
+[ 
+["units" => "1", "name" => "Skeleton", "hpscale" => "2", "delay" => "10"],
+["units" => "1", "name" => "Armored Skeleton", "hpscale" => "2", "delay" => "20"],
+["units" => "2", "name" => "Skeleton", "hpscale" => "1", "delay" => "5"],
+["units" => "1", "name" => "Armored Skeleton", "hpscale" => "1", "delay" => "8"],
+["units" => "1", "name" => "Armored Skeleton", "hpscale" => "1", "delay" => "20"],
+["units" => "1", "name" => "Wolf", "hpscale" => "1", "delay" => "1"],
+["units" => "3", "name" => "Wolf", "hpscale" => "1", "delay" => "1"],
+["units" => "1", "name" => "Wolf", "hpscale" => "1", "delay" => "2"],
+];
+
+//Round 5
+_Enemies =
+[ 
+["units" => "1", "name" => "Armored Skeleton", "hpscale" => "2", "delay" => "20"],
+["units" => "2", "name" => "Skeleton", "hpscale" => "2", "delay" => "20"],
+["units" => "2", "name" => "Skeleton", "hpscale" => "1", "delay" => "5"],
+["units" => "1", "name" => "Armored Skeleton", "hpscale" => "1", "delay" => "8"],
+["units" => "1", "name" => "Armored Skeleton", "hpscale" => "1", "delay" => "10"],
+["units" => "1", "name" => "Skeleton", "hpscale" => "2", "delay" => "7"],
+["units" => "1", "name" => "Armored Skeleton", "hpscale" => "2", "delay" => "30"],
+["units" => "1", "name" => "Skeleton", "hpscale" => "4", "delay" => "10"],
+];
+
+//Round 6
+_Enemies =
+[ 
+["units" => "3", "name" => "Wolf", "hpscale" => "1", "delay" => "2"],
+["units" => "2", "name" => "Wolf", "hpscale" => "1", "delay" => "15"],
+["units" => "1", "name" => "Skeleton", "hpscale" => "4", "delay" => "5"],
+["units" => "1", "name" => "Skeleton", "hpscale" => "2", "delay" => "5"],
+["units" => "1", "name" => "Armored Skeleton", "hpscale" => "4", "delay" => "10"],
+["units" => "2", "name" => "Skeleton", "hpscale" => "2", "delay" => "7"],
+["units" => "2", "name" => "Armored Skeleton", "hpscale" => "1", "delay" => "20"],
+["units" => "1", "name" => "Skeleton", "hpscale" => "4", "delay" => "5"],
+["units" => "2", "name" => "Wolf", "hpscale" => "1", "delay" => "2"],
+["units" => "1", "name" => "Wolf", "hpscale" => "1", "delay" => "0"],
+];
+
+//Round 7
+_Enemies =
+[ 
+["units" => "1", "name" => "Armored Skeleton", "hpscale" => "4", "delay" => "9"],
+["units" => "2", "name" => "Wolf", "hpscale" => "1", "delay" => "10"],
+["units" => "2", "name" => "Skeleton", "hpscale" => "4", "delay" => "5"],
+["units" => "1", "name" => "Skeleton", "hpscale" => "4", "delay" => "15"],
+["units" => "1", "name" => "Armored Skeleton", "hpscale" => "4", "delay" => "10"],
+["units" => "1", "name" => "Skeleton", "hpscale" => "4", "delay" => "7"],
+["units" => "1", "name" => "Armored Skeleton", "hpscale" => "2", "delay" => "20"],
+["units" => "1", "name" => "Beast", "hpscale" => "1", "delay" => "5"],
+];
+
+//Round 8
+_Enemies =
+[ 
+["units" => "3", "name" => "Skeleton", "hpscale" => "4", "delay" => "12"],
+["units" => "1", "name" => "Beast", "hpscale" => "1", "delay" => "20"],
+["units" => "1", "name" => "Armored Skeleton", "hpscale" => "4", "delay" => "10"],
+["units" => "2", "name" => "Wolf", "hpscale" => "1", "delay" => "3"],
+["units" => "2", "name" => "Wolf", "hpscale" => "1", "delay" => "10"],
+["units" => "1", "name" => "Beast", "hpscale" => "1", "delay" => "15"],
+["units" => "1", "name" => "Skeleton", "hpscale" => "4", "delay" => "20"],
+["units" => "1", "name" => "Armored Skeleton", "hpscale" => "4", "delay" => "0"],
+];
+
+//Round 9
+_Enemies =
+[ 
+["units" => "1", "name" => "Beast", "hpscale" => "1", "delay" => "12"],
+["units" => "1", "name" => "Skeleton", "hpscale" => "6", "delay" => "15"],
+["units" => "1", "name" => "Armored Skeleton", "hpscale" => "6", "delay" => "9"],
+["units" => "3", "name" => "Wolf", "hpscale" => "1", "delay" => "20"],
+["units" => "1", "name" => "Beast", "hpscale" => "1", "delay" => "8"],
+["units" => "1", "name" => "Beast", "hpscale" => "1", "delay" => "15"],
+["units" => "1", "name" => "Skeleton", "hpscale" => "4", "delay" => "12"],
+["units" => "1", "name" => "Skeleton", "hpscale" => "6", "delay" => "0"],
+];
+
+//Round 10
+_Enemies =
+[ 
+["units" => "1", "name" => "Beast", "hpscale" => "1", "delay" => "20"],
+["units" => "1", "name" => "Skeleton", "hpscale" => "6", "delay" => "20"],
+["units" => "1", "name" => "Beast", "hpscale" => "1", "delay" => "9"],
+["units" => "1", "name" => "Armored Skeleton", "hpscale" => "6", "delay" => "20"],
+["units" => "3", "name" => "Wolf", "hpscale" => "1", "delay" => "3"],
+["units" => "2", "name" => "Wolf", "hpscale" => "1", "delay" => "9"],
+["units" => "2", "name" => "Armored Skeleton", "hpscale" => "6", "delay" => "8"],
+["units" => "2", "name" => "Skeleton", "hpscale" => "6", "delay" => "15"],
+["units" => "1", "name" => "Brute", "hpscale" => "1", "delay" => "0"],
+];
